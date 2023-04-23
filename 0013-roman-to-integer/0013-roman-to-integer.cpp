48a264f288af9c010b825df48c318932b11d1219
Akaash2409 <130816130+Akaash2409@users.noreply.github.com>
2023-04-23 13:03:06 +0530

    Time: 4 ms (91.65%), Space: 6 MB (92.82%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int intNum = 0;
+        int prevVal = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            int currVal = getIntVal(s[i]);
+            if (prevVal >= currVal)
+                intNum += currVal;
+            else
+            {
+                intNum -= prevVal;
+                intNum += currVal - prevVal;
+            }
+            prevVal = currVal;
+        }
+        return intNum;
+    }
+    
+    int getIntVal(char sym) {
+        switch (sym) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
+};
