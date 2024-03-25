61ef7f71331fa9afa7270c055bf9f6c40cf699fd
Akaash Sharma <130816130+Akaash2409@users.noreply.github.com>
2024-03-25 22:26:28 +0530

    Time: 37 ms (81.09%), Space: 35.9 MB (77.98%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> result; // Result vector to store duplicate numbers
+        vector<int> result;
         for (int i = 0; i < nums.size(); ++i) {
-            int index = abs(nums[i]) - 1; // Get the index corresponding to the number
-            if (nums[index] < 0) { // If the number at index is negative, it means it's seen before
-                result.push_back(index + 1); // Add the duplicate number to the result
-            } else {
-                nums[index] = -nums[index]; // Mark the number as seen by negating it
+            int index = abs(nums[i]) - 1; 
+            if (nums[index] < 0) { 
+                result.push_back(index + 1);
+            }
+            else {
+                nums[index] = -nums[index]; 
             }
         }
-        return result; // Return the vector containing duplicate numbers
+        return result; 
     }
 };
